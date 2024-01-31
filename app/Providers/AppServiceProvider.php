53c61cb1002357d3788d2f479ff7f5c39bc5c54f
thumptraining <95773873+thumptraining@users.noreply.github.com>
2024-01-31 01:30:07 -0300

    Force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,8 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (env('APP_ENV') === 'production') {
-            \Illuminate\Support\Facades\URL::forceScheme('https');
+        if(env('FORCE_HTTPS',false)) { // Default value should be false for local server
+            URL::forceScheme('https');
         }
     }
 }
